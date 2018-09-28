e0b63d9de8c0ecae7ecb6fc936e8ad4ac2ad6d9e
eienEika <dmatro86@gmail.com>
2018-09-28 03:03:38 +0300

    Getting files

--- bot.py
+++ bot.py
@@ -1,9 +1,28 @@
 import config
 import telebot
+import numpy
+import os
 
-bot = telebot.Telebot(config.token)
+bot = telebot.TeleBot(config.token)
+
 
 @bot.message_handler(content_types=["text"])
 def repeat(message):
 	bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(content_types=['document'])
+def files(message):
+	file_info = bot.get_file(message.document.file_id)
+	downloaded_file = bot.download_file(file_info.file_path)
+
+	src = 'D:\\qot\\' + message.document.file_name
+	with open(src, 'w') as file:
+		file.write(downloaded_file)
+
+	bot.reply_to(message, "Файл добавлено")
+
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
 
