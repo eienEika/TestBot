98055c592e9404f1799a3428e3a7d1f7b006b460
eienEika <dmatro86@gmail.com>
2018-09-28 11:08:43 +0300

    More convinient work with dirs

--- bot.py
+++ bot.py
@@ -2,13 +2,20 @@ import config
 import telebot
 import os
 import solve
+import time
 
 bot = telebot.TeleBot(config.token)
 
+# path to downloaded files
+path = os.environ['HOMEPATH']
+if not os.path.exists(path + os.sep + 'tmpBOT'):
+	os.mkdir(path + os.sep + 'tmpBOT')
+path += os.sep + 'tmpBOT' + os.sep
 
-@bot.message_handler(content_types=["text"])
-def repeat(message):
-	bot.send_message(message.chat.id, message.text)
+
+# @bot.message_handler(content_types=["text"])
+# def repeat(message):
+#	bot.send_message(message.chat.id, message.text)
 
 
 @bot.message_handler(content_types=['document'])
@@ -16,21 +23,27 @@ def get_file(message):
 	file_info = bot.get_file(message.document.file_id)
 	downloaded_file = bot.download_file(file_info.file_path)
 
-	src = 'D:\\qot\\' + message.document.file_name
+	file_name = str(time.time())    # creating unique dir and file names
+	file_dir = path + file_name
+	os.mkdir(file_dir)
+	src = file_dir + os.sep + file_name
 	with open(src, 'wb') as file:
 		file.write(downloaded_file)
 	file.close()
 	bot.reply_to(message, "Файл добавлено")
 
 	out = solve.solve(src)[0]
-	if len(out) > 10:
-		out_file = open('D:\\qot\\out.txt', 'w+')
+	os.remove(src)
+
+	if len(out) > 10:   # if system is big then bot will send you answer in out file
+		out_file = open(file_dir + os.sep + 'out.txt', 'w+')
 		for i in out:
 			out_file.write(str(i) + '\n')
-		out_file = open('D:\\qot\\out.txt', 'r')
+		out_file = open(file_dir + os.sep + 'out.txt', 'r')
 		bot.send_document(message.chat.id, out_file)
 		out_file.close()
-		os.remove('D:\\qot\\out.txt')
+		os.remove(file_dir + os.sep + 'out.txt')
+		os.rmdir(file_dir)
 	else:
 		out_message = ''
 		for i in out:
